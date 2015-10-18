58de6c0a5f43da30b5966577e532438fe4fa1d70
Lyosha Kuleshov <aleksey.kuleshov@flocktory.com>
2015-10-18 16:17:47 +0300

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovation "0.1.0-SNAPSHOT"
+(defproject ovation "0.1.0"
   :description "Novation controllers interface for Overtone"
   :url "http://github.com/superkonduktr/ovation"
   :license {:name "Eclipse Public License"
